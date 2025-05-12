16a15f5a395bafeab2052648b3f629f6216c842e
Atan0707 <harizhakim85@gmail.com>
2025-05-12 14:12:50 +0800

    refactor navbar

--- src/app/stamp/page.jsx
+++ src/app/stamp/page.jsx
@@ -1,26 +1,13 @@
 'use client';
 import { useState } from 'react';
 import StampCard from '../components/StampCard';
-import { useWallet } from '@solana/wallet-adapter-react';
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { WalletProvider } from '../components/wallet-provider';
 
 export default function StampPage() {
   const [showConnected, setShowConnected] = useState(false);
   
   return (
-    <WalletProvider>
+
       <div className="min-h-screen bg-gray-100">
-        {/* Navigation */}
-        <nav className="flex justify-between items-center p-4 bg-[#1A2A3A]">
-          <div className="flex items-center space-x-4">
-            <a href="/" className="nav-link">HOME</a>
-            <a href="/#about-us" className="nav-link">ABOUT US</a>
-          </div>
-          <div className="wallet-adapter-dropdown">
-            <WalletMultiButton className="wallet-adapter-button" />
-          </div>
-        </nav>
         
         <div className="py-12 px-4">
           <div className="max-w-4xl mx-auto">
@@ -37,6 +24,6 @@ export default function StampPage() {
           </div>
         </div>
       </div>
-    </WalletProvider>
+
   );
 } 
